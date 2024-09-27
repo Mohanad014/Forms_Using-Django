839c49612fdce9770d07257947d30d405c04cd95
Mohan A  D <125897196+Mohanad014@users.noreply.github.com>
2024-09-27 10:35:33 +0530

    Update urls.py

--- model_form/urls.py
+++ model_form/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('mform/',views.user_input, name= 'Model_Input'),
-    path('moutput/',views.user_output, name= 'Model_Output'),
+    path('moutput/',views.user_output, name= 'Model_Output'), 
 ]
